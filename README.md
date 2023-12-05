2417e4e810c5a47afeb66ba734b3bac5c884bbed
barami62 <wodbs7758@naver.com>
2023-12-05 17:52:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# today-code-complete
---
+++ src/main/java/sssdev/tcc/TccApplication.java
@@ -0,0 +1,15 @@
+package sssdev.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class TccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccApplication.class, args);
+	}
+}

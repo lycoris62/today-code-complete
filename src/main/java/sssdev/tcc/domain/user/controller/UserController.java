89c3569ca39986210baee173c67875d1b184cca2
yudonggeun <ydong98@gmail.com>
2023-12-06 15:30:57 +0900

    feat(user): 프로필 조회 구현-1
    - 프로필 조회 성공 테스트 작성
    - 프로필 조회 성공 케이스 구현

--- src/main/java/sssdev/tcc/domain/user/controller/UserController.java
+++ src/main/java/sssdev/tcc/domain/user/controller/UserController.java
@@ -1,5 +1,29 @@
 package sssdev.tcc.domain.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sssdev.tcc.domain.user.dto.response.ProfileResponse;
+import sssdev.tcc.domain.user.service.UserService;
+import sssdev.tcc.global.common.dto.response.RootResponse;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping("/{id}/profile")
+    public ResponseEntity<?> getProfile(@PathVariable(name = "id") Long id) {
+        ProfileResponse response = userService.getProfile(id);
+        return ResponseEntity.ok(RootResponse.builder()
+            .code("200")
+            .message("성공했습니다.")
+            .data(response)
+            .build());
+    }
 }
---
+++ src/test/java/sssdev/tcc/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,51 @@
+package sssdev.tcc.domain.user.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import sssdev.tcc.domain.user.dto.response.ProfileResponse;
+import sssdev.tcc.domain.user.service.UserService;
+import sssdev.tcc.support.ControllerTest;
+
+@DisplayName("유저 API 서비스")
+class UserControllerTest extends ControllerTest {
+
+    @MockBean
+    UserService userService;
+
+    @Nested
+    @DisplayName("프로필 단건 조회")
+    class ProfileGet {
+
+        @DisplayName("성공 케이스")
+        @Test
+        void success() throws Exception {
+            // given
+            var userId = 1L;
+            var response = new ProfileResponse("test", 100, 200, "/api/test/image.png",
+                "description");
+            given(userService.getProfile(userId)).willReturn(response);
+            // when // then
+            mockMvc.perform(get("/api/users/{userId}/profile", userId))
+                .andDo(print())
+                .andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.code").value("200"),
+                    jsonPath("$.message").value("성공했습니다."),
+                    jsonPath("$.data.nickname").value(response.nickname()),
+                    jsonPath("$.data.followerCount").value(response.followerCount()),
+                    jsonPath("$.data.followingCount").value(response.followingCount()),
+                    jsonPath("$.data.profileImageUrl").value(response.profileImageUrl()),
+                    jsonPath("$.data.description").value(response.description())
+                );
+        }
+    }
+}

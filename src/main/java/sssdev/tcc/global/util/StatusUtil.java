2527be055605a9f2b5eb63096c8e91caeeb3b5d7
yudonggeun <ydong98@gmail.com>
2023-12-06 20:07:04 +0900

    feat(user): 로그인 유저 상태 정보 조회 기능 구현

--- src/main/java/sssdev/tcc/global/util/StatusUtil.java
+++ src/main/java/sssdev/tcc/global/util/StatusUtil.java
@@ -1,5 +1,22 @@
 package sssdev.tcc.global.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import sssdev.tcc.global.common.dto.LoginUser;
+import sssdev.tcc.global.execption.ErrorCode;
+import sssdev.tcc.global.execption.ServiceException;
+
+@Component
 public class StatusUtil {
 
+    private final String LOGIN_USER = "login_user";
+
+    public LoginUser getLoginUser(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new ServiceException(ErrorCode.NOT_LOGIN);
+        }
+        return (LoginUser) session.getAttribute(LOGIN_USER);
+    }
 }

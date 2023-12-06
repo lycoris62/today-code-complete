86c0c6d685c97c941b515468a7f88e976ed7b331
barami62 <wodbs7758@naver.com>
2023-12-06 22:04:01 +0900

    feat(post): 게시글 목록 조회 API 추가
    - query가 빈 값이면 전체 조회, 존재하면 포함 게시글 조회

--- src/main/java/sssdev/tcc/domain/post/controller/PostController.java
+++ src/main/java/sssdev/tcc/domain/post/controller/PostController.java
@@ -1,5 +1,38 @@
 package sssdev.tcc.domain.post.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sssdev.tcc.domain.post.dto.response.PostDetailResponse;
+import sssdev.tcc.domain.post.service.PostService;
+import sssdev.tcc.global.common.dto.response.RootResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
 public class PostController {
 
+    private final PostService postService;
+
+    /**
+     * 게시글 목록 조회
+     *
+     * @param query 값이 있다면 query 포함 게시글 목록 반환
+     */
+    @GetMapping
+    public ResponseEntity<RootResponse<Page<PostDetailResponse>>> getPosts(
+        Pageable pageable,
+        @RequestParam(name = "query", required = false, defaultValue = "") String query) {
+
+        Page<PostDetailResponse> postList = postService.getPosts(pageable, query);
+
+        return ResponseEntity.ok(RootResponse.<Page<PostDetailResponse>>builder()
+            .data(postList)
+            .build());
+    }
 }

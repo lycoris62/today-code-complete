0f926d7c8f8d0aa0804ec582dbcc3ede35f73ad2
yudonggeun <ydong98@gmail.com>
2023-12-06 16:44:51 +0900

    feat(exception): 에러 핸들링 구현
    - 에러 상태 enum 클래스 생성
    - global error handler  구현
    - 커스텀 서비스 에러 Exception 구현

--- src/main/java/sssdev/tcc/global/execption/GlobalExceptionController.java
+++ src/main/java/sssdev/tcc/global/execption/GlobalExceptionController.java
@@ -1,5 +1,21 @@
 package sssdev.tcc.global.execption;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sssdev.tcc.global.common.dto.response.RootResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionController {
 
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<?> handleException(ServiceException ex) {
+        ErrorCode code = ex.getCode();
+        RootResponse<Object> response = RootResponse.builder()
+            .code(code.getCode())
+            .message(code.getMessage())
+            .build();
+        return ResponseEntity.status(code.getStatus())
+            .body(response);
+    }
 }

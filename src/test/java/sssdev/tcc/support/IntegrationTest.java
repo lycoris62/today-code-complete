3c6a27bd41581a785a6c52c3af2e584bfaf26385
yudonggeun <ydong98@gmail.com>
2023-12-06 15:39:48 +0900

    test(support): 테스트 실행 환경 통합 설정
    - 통합 테스트 환경 설정 구현
    - Repository 테스트 환경 설정 구현

---
+++ src/test/java/sssdev/tcc/support/IntegrationTest.java
@@ -0,0 +1,17 @@
+package sssdev.tcc.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Autowired
+    protected MockMvc mockMvc;
+}

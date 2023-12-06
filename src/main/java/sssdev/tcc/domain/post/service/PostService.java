c39d05b2487732652884f03668e80a9b2fbf7d02
barami62 <wodbs7758@naver.com>
2023-12-06 22:02:07 +0900

    feat(post): Post Service 전체 게시글 조회 메서드 추가
    - query가 빈 값이 아니면 특정 query 포함 게시글 목록 조회

--- src/main/java/sssdev/tcc/domain/post/service/PostService.java
+++ src/main/java/sssdev/tcc/domain/post/service/PostService.java
@@ -1,5 +1,31 @@
 package sssdev.tcc.domain.post.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import sssdev.tcc.domain.post.dto.response.PostDetailResponse;
+import sssdev.tcc.domain.post.repository.PostRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class PostService {
 
+    private final PostRepository postRepository;
+
+    /**
+     * 모든 게시글을 가져옴. query 값이 존재하면 query 포함 게시글을 가져옴
+     */
+    public Page<PostDetailResponse> getPosts(Pageable pageable, String query) {
+
+        if (query.isBlank()) {
+            return postRepository.findAll(pageable)
+                .map(PostDetailResponse::of);
+        }
+
+        return postRepository.findAllByContentContaining(query, pageable)
+            .map(PostDetailResponse::of);
+    }
 }

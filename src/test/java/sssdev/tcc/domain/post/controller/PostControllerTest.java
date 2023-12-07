5e612cfd5fd7135a4c4bd88e804e45321ab34ac3
barami62 <wodbs7758@naver.com>
2023-12-07 14:52:48 +0900

    test(post): post controller 테스트 수정
    - 필요한 Mock 추가

--- src/test/java/sssdev/tcc/domain/post/controller/PostControllerTest.java
+++ src/test/java/sssdev/tcc/domain/post/controller/PostControllerTest.java
@@ -1,8 +1,10 @@
 package sssdev.tcc.domain.post.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -44,13 +47,25 @@ class PostControllerTest extends ControllerTest {
     @DisplayName("게시글 목록 조회")
     class PostGet {
 
+        String jsonPath = "$.data.content[?(@.content == '%s')]";
+
+        @BeforeEach
+        void setUp() {
+            given(commentRepository.countByPostId(anyLong())).willReturn(0L);
+            given(postLikeRepository.countByPostId(anyLong())).willReturn(0L);
+            given(postLikeRepository.existsByUserIdAndPostId(anyLong(), anyLong()))
+                .willReturn(false);
+        }
+
         @DisplayName("성공 케이스 - 전체 게시글")
         @Test
         void get_posts_success() throws Exception {
             // given
             User user = User.builder().username("username").build();
             Post post1 = Post.builder().content("content01").user(user).build();
+            setField(post1, "id", 1L);
             Post post2 = Post.builder().content("content02").user(user).build();
+            setField(post2, "id", 2L);
             PostDetailResponse postDetail1 = PostDetailResponse.of(post1, commentRepository,
                 postLikeRepository);
             PostDetailResponse postDetail2 = PostDetailResponse.of(post2, commentRepository,
@@ -64,8 +79,6 @@ class PostControllerTest extends ControllerTest {
 
             given(postService.getPosts(any(Pageable.class), anyString())).willReturn(pageResult);
 
-            String jsonPath = "$.data.content[?(@.content == '%s')]";
-
             // when // then
             mockMvc.perform(get("/api/posts")
                     .accept(MediaType.APPLICATION_JSON)
@@ -77,9 +90,8 @@ class PostControllerTest extends ControllerTest {
                     jsonPath("$.data.content.size()").value(2),
                     jsonPath(jsonPath + ".username", "content01").value(postDetail1.username()),
                     jsonPath(jsonPath + ".nickname", "content01").value(postDetail1.nickname()),
-                    jsonPath(jsonPath + ".commentCount", "content01").value(
-                        postDetail1.commentCount()),
-                    jsonPath(jsonPath + ".likeCount", "content01").value(postDetail1.likeCount()),
+                    jsonPath(jsonPath + ".commentCount", "content01").value(0),
+                    jsonPath(jsonPath + ".likeCount", "content01").value(0),
                     jsonPath(jsonPath + ".content", "content01").value(postDetail1.content()),
                     jsonPath(jsonPath + ".content", "content02").value(postDetail2.content())
                 );
@@ -92,7 +104,9 @@ class PostControllerTest extends ControllerTest {
             String query = "01";
             User user = User.builder().username("username").build();
             Post post1 = Post.builder().content("content01").user(user).build();
+            setField(post1, "id", 1L);
             Post post2 = Post.builder().content("content02").user(user).build();
+            setField(post2, "id", 2L);
             PostDetailResponse postDetail1 = PostDetailResponse.of(post1, commentRepository,
                 postLikeRepository);
             PostDetailResponse postDetail2 = PostDetailResponse.of(post2, commentRepository,
@@ -108,8 +122,6 @@ class PostControllerTest extends ControllerTest {
 
             given(postService.getPosts(any(Pageable.class), anyString())).willReturn(pageResult);
 
-            String jsonPath = "$.data.content[?(@.content == '%s')]";
-
             // when // then
             mockMvc.perform(get("/api/posts")
                     .accept(MediaType.APPLICATION_JSON)
@@ -122,9 +134,8 @@ class PostControllerTest extends ControllerTest {
                     jsonPath("$.data.content.size()").value(1),
                     jsonPath(jsonPath + ".username", "content01").value(postDetail1.username()),
                     jsonPath(jsonPath + ".nickname", "content01").value(postDetail1.nickname()),
-                    jsonPath(jsonPath + ".commentCount", "content01").value(
-                        postDetail1.commentCount()),
-                    jsonPath(jsonPath + ".likeCount", "content01").value(postDetail1.likeCount()),
+                    jsonPath(jsonPath + ".commentCount", "content01").value(0),
+                    jsonPath(jsonPath + ".likeCount", "content01").value(0),
                     jsonPath(jsonPath + ".content", "content01").value(postDetail1.content())
                 );
         }

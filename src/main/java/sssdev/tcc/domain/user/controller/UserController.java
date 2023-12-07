8ee132f8b6a5e8e9467acf426ca718677d2d619e
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-12-07 12:36:28 +0900

    feat(test): 테스트 에러코드 수정
    - 테스트 에러코드 수정

--- src/main/java/sssdev/tcc/domain/user/controller/UserController.java
+++ src/main/java/sssdev/tcc/domain/user/controller/UserController.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +34,7 @@ public class UserController {
             .build());
     }
 
-    @PostMapping("/profile")
+    @PatchMapping("/profile")
     public ResponseEntity<?> updateProfile(@RequestBody ProfileUpdateRequest body,
         HttpServletRequest request) {
         LoginUser loginUser = statusUtil.getLoginUser(request);
@@ -45,4 +45,5 @@ public class UserController {
             .data(response)
             .build());
     }
+
 }

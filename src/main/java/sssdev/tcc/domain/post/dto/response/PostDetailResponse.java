94a50a4ac6199d6028ff1299772322587e6357d9
barami62 <wodbs7758@naver.com>
2023-12-07 12:55:47 +0900

    fix(post): 게시글 반환 DTO 수정
    - 반환 타입을 기본형 타입으로 변경
    - 정수 반환 타입을 long으로 변경
    - 좋아요 개수를 레포지토리에서 가져오도록 수정
    - 댓글 개수를 레포지토리에서 가져오도록 수정
    - 좋아요 클릭 여부를 레포지토리에서 가져오도록 수정

--- src/main/java/sssdev/tcc/domain/post/dto/response/PostDetailResponse.java
+++ src/main/java/sssdev/tcc/domain/post/dto/response/PostDetailResponse.java
@@ -1,27 +1,32 @@
 package sssdev.tcc.domain.post.dto.response;
 
+import sssdev.tcc.domain.comment.repository.CommentRepository;
 import sssdev.tcc.domain.post.domain.Post;
+import sssdev.tcc.domain.post.repository.PostLikeRepository;
 
 public record PostDetailResponse(
-    Long postId,
+    long postId,
     String username,
     String nickname,
     String content,
-    Integer commentCount,
-    Integer likeCount,
-    Boolean isLike
+    long commentCount,
+    long likeCount,
+    boolean isLike
 ) {
 
-    public static PostDetailResponse of(Post post) {
+    public static PostDetailResponse of(
+        Post post,
+        CommentRepository commentRepository,
+        PostLikeRepository postLikeRepository) {
+
         return new PostDetailResponse(
             post.getId(),
             post.getUser().getUsername(),
             post.getUser().getNickname(),
             post.getContent(),
-            post.getCommentList().size(),
-            post.getPostLikeList().size(),
-            post.getPostLikeList().stream()
-                .anyMatch(postLike -> postLike.getUser().getId().equals(post.getUser().getId()))
+            post.getCommentCount(commentRepository),
+            post.getLikeCount(postLikeRepository),
+            post.getIsLike(postLikeRepository)
         );
     }
 }

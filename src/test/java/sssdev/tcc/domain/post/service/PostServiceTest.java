dc8a0b95f1fb863cb800638839c500045f258166
barami62 <wodbs7758@naver.com>
2023-12-07 10:54:34 +0900

    test(post): 게시글 목록 조회 테스트 추가
    - PostService 전체 게시글 조회 테스트 추가
    - PostService 검색 게시글 조회 테스트 추가

---
+++ src/test/java/sssdev/tcc/domain/post/service/PostServiceTest.java
@@ -0,0 +1,88 @@
+package sssdev.tcc.domain.post.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import sssdev.tcc.domain.post.domain.Post;
+import sssdev.tcc.domain.post.dto.response.PostDetailResponse;
+import sssdev.tcc.domain.post.repository.PostRepository;
+import sssdev.tcc.domain.user.domain.User;
+
+@DisplayName("게시글 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+    @Mock
+    PostRepository postRepository;
+
+    @InjectMocks
+    PostService postService;
+
+    @Nested
+    @DisplayName("게시글 목록 조회")
+    class PostGet {
+
+        @DisplayName("성공 케이스 - 전체 게시글")
+        @Test
+        void get_posts_success() {
+            // given
+            User user = User.builder().username("username").build();
+            Post post1 = Post.builder().content("content01").user(user).build();
+            Post post2 = Post.builder().content("content02").user(user).build();
+            Pageable pageable = PageRequest.of(0, 10);
+
+            Page<Post> pageResult = new PageImpl<>(List.of(post1, post2), pageable, 2);
+
+            given(postRepository.findAll(pageable)).willReturn(pageResult);
+
+            // when
+            Page<PostDetailResponse> posts = postService.getPosts(pageable, "");
+
+            // then
+            assertThat(posts.getContent().size()).isEqualTo(2);
+            assertThat(posts.getContent().get(0).content()).isEqualTo(post1.getContent());
+            assertThat(posts.getContent().get(1).content()).isEqualTo(post2.getContent());
+            assertThat(posts.getContent().get(0).username()).isEqualTo(user.getUsername());
+        }
+
+        @DisplayName("성공 케이스 - 검색 게시글")
+        @Test
+        void get_posts_by_query_success() {
+            // given
+            User user = User.builder().username("username").build();
+            Post post1 = Post.builder().content("content01").user(user).build();
+            Post post2 = Post.builder().content("content02").user(user).build();
+            Pageable pageable = PageRequest.of(0, 10);
+            String query = "01";
+
+            Page<Post> pageResult = new PageImpl<>(
+                Stream.of(post1, post2).filter(post -> post.getContent().contains(query)).toList(),
+                pageable,
+                2);
+
+            given(postRepository.findAllByContentContaining(query, pageable))
+                .willReturn(pageResult);
+
+            // when
+            Page<PostDetailResponse> posts = postService.getPosts(pageable, query);
+
+            // then
+            assertThat(posts.getContent().size()).isEqualTo(1);
+            assertThat(posts.getContent().get(0).content()).isEqualTo(post1.getContent());
+            assertThat(posts.getContent().get(0).username()).isEqualTo(user.getUsername());
+        }
+    }
+}

2d29798b7346b5b45b8641be72b9bd768ea1050e
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-08 17:47:01 +0900

    feature: 로그인 후 특정 Post의 댓글을 전체 조회할 경우의 테스트 API 구현

--- src/test/java/sssdev/tcc/domain/comment/controller/CommentControllerTest.java
+++ src/test/java/sssdev/tcc/domain/comment/controller/CommentControllerTest.java
@@ -1,6 +1,7 @@
 package sssdev.tcc.domain.comment.controller;
 
 import static org.mockito.BDDMockito.*;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -17,6 +18,8 @@ import org.springframework.http.MediaType;
 import sssdev.tcc.domain.comment.dto.request.CommentRequest;
 import sssdev.tcc.domain.comment.dto.response.CommentResponse;
 import sssdev.tcc.domain.comment.service.CommentService;
+import sssdev.tcc.domain.user.domain.User;
+import sssdev.tcc.global.common.dto.LoginUser;
 import sssdev.tcc.global.util.StatusUtil;
 import sssdev.tcc.support.ControllerTest;
 
@@ -47,26 +50,55 @@ class CommentControllerTest extends ControllerTest {
 
             String json = objectMapper.writeValueAsString(request);
 
-            given(statusUtil.loginStatus(any(HttpServletRequest.class))).willReturn(false);
-            given(commentService.getCommentsNonLogin(any())).willReturn(responseList);
+            given(statusUtil.loginStatus(any())).willReturn(false);
+            given(commentService.getCommentsNonLogin(request.postId())).willReturn(responseList);
 
-            mockMvc.perform(get("/api/comments")
-                    .content(json)
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpectAll(
-                    status().isOk(),
+            mockMvc.perform(
+                    get("/api/comments").content(json).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpectAll(status().isOk(),
                     jsonPath("$.data.size()").value(3),
                     jsonPath("$.data[0].content").value("댓글 내용 0"),
                     jsonPath("$.data[1].content").value("댓글 내용 1"),
-                    jsonPath("$.data[2].content").value("댓글 내용 2")
-                );
+                    jsonPath("$.data[2].content").value("댓글 내용 2"));
         }
 
         @Test
         @DisplayName("로그인을 했을 때 전체 조회 할 경우")
-        void get_comments_test_login() {
+        void get_comments_test_login() throws Exception {
+            LoginUser loginUser = new LoginUser(1L);
 
+            User user = User.builder().username("작성자").build();
+            setField(user, "id", 1L);
+
+            List<CommentResponse> responseList = new ArrayList<>();
+            CommentRequest request = new CommentRequest(1L);
+
+            for (int i = 0; i < 3; i++) {
+                CommentResponse response = new CommentResponse(user.getUsername(), "댓글 내용 " + i,
+                    i % 2 == 0);
+                responseList.add(response);
+            }
+
+            String json = objectMapper.writeValueAsString(request);
+
+            given(statusUtil.loginStatus(any())).willReturn(true);
+            given(statusUtil.getLoginUser(any())).willReturn(loginUser);
+            given(commentService.getCommentsLogin(request.postId(), any()))
+                .willReturn(responseList);
+
+            mockMvc.perform(
+                get("/api/comments")
+                    .content(json)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                .andDo(print())
+                .andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.data.size()").value(3),
+                    jsonPath("$.data[0].likeStatus").value(true),
+                    jsonPath("$.data[0].likeStatus").value(false),
+                    jsonPath("$.data[0].likeStatus").value(true)
+                );
         }
     }
 }

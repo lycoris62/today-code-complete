117ec78c77c9fae6d607e157c141b6ed8c77bf48
barami62 <wodbs7758@naver.com>
2023-12-08 21:47:12 +0900

    feat(follow): repository에 pageable 추가

--- src/main/java/sssdev/tcc/domain/user/repository/FollowRepository.java
+++ src/main/java/sssdev/tcc/domain/user/repository/FollowRepository.java
@@ -1,6 +1,7 @@
 package sssdev.tcc.domain.user.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +14,5 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     long countFollowingByFromId(Long from_id);
 
     @Query("select u.id from Follow f join User u on f.to.id = u.id where f.from.id = :fromId")
-    List<Long> findAllFollowIdByFromId(@Param("fromId") Long fromId);
+    List<Long> findAllFollowIdByFromId(@Param("fromId") Long fromId, Pageable pageable);
 }

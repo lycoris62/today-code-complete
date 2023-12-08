f759a5770f4198340e95971277f5ce631eca814d
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-12-08 17:20:29 +0900

    refactor(admin) : 어드민 컨트롤러 및 테스트 코드 수정
    어드민 컨트롤러 및 테스트 코드 수정

--- src/main/java/sssdev/tcc/domain/comment/service/CommentService.java
+++ src/main/java/sssdev/tcc/domain/comment/service/CommentService.java
@@ -1,5 +1,17 @@
 package sssdev.tcc.domain.comment.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
 
+    // todo
+    public void deleteComment(Long id) {
+    }
+
+    // todo
+    public void deletePost(Long id) {
+
+    }
 }

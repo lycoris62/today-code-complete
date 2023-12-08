84fa6ca43800d6bc725e6571d5025ef97fb8e98b
barami62 <wodbs7758@naver.com>
2023-12-08 20:47:25 +0900

    feat(follow): 팔로우 중인 유저의 id 리스트 반환
    - 특정 유저의 id를 넣으면 그 유저가 팔로우 중인 유저의 id 리스트를 반환하는 메서드 추가

--- src/main/java/sssdev/tcc/domain/user/repository/FollowRepository.java
+++ src/main/java/sssdev/tcc/domain/user/repository/FollowRepository.java
@@ -1,6 +1,9 @@
 package sssdev.tcc.domain.user.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sssdev.tcc.domain.user.domain.Follow;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
@@ -8,4 +11,7 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     long countFollowerByToId(Long from_id);
 
     long countFollowingByFromId(Long from_id);
+
+    @Query("select u.id from Follow f join User u on f.to.id = u.id where f.from.id = :fromId")
+    List<Long> findAllFollowIdByFromId(@Param("fromId") Long fromId);
 }

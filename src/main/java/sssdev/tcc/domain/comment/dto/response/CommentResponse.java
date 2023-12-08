26646ea52a39d601291eb489d540bdd78b48123e
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-08 19:51:33 +0900

    refactory: Add @Builder in CommentResponse.java

--- src/main/java/sssdev/tcc/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/sssdev/tcc/domain/comment/dto/response/CommentResponse.java
@@ -1,9 +1,12 @@
 package sssdev.tcc.domain.comment.dto.response;
 
+import lombok.Builder;
+import sssdev.tcc.domain.comment.domain.Comment;
+
+@Builder
 public record CommentResponse(
     String writer,
     String content,
     Boolean likeStatus
 ) {
-
 }

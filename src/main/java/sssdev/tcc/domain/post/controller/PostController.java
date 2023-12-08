342c041e8aae06f5795d48c7b0ee5d980c54c270
barami62 <wodbs7758@naver.com>
2023-12-08 22:56:30 +0900

    feat(post): 게시글 생성 API 추가

--- src/main/java/sssdev/tcc/domain/post/controller/PostController.java
+++ src/main/java/sssdev/tcc/domain/post/controller/PostController.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sssdev.tcc.domain.post.dto.request.PostCreateRequest;
 import sssdev.tcc.domain.post.dto.response.PostDetailResponse;
 import sssdev.tcc.domain.post.service.PostService;
 import sssdev.tcc.global.common.dto.LoginUser;
@@ -54,5 +56,19 @@ public class PostController {
         return ResponseEntity.ok(RootResponse.<Page<PostDetailResponse>>builder()
             .data(postList)
             .build());
+    }
+
+    /**
+     * 게시글 생성
+     */
+    @PostMapping
+    public ResponseEntity<?> createPost(
+        PostCreateRequest requestDto,
+        HttpServletRequest servletRequest) {
+
+        LoginUser loginUser = statusUtil.getLoginUser(servletRequest);
+        postService.createPost(loginUser, requestDto);
+
+        return ResponseEntity.ok().build();
     }
 }

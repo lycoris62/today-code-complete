69d5b8bf0ccd7126c0079c3d7540dd2ae876dabd
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-08 19:55:04 +0900

    feature: 로그인 하지 않은 상태로 해당 Post 댓글 전체 조회 API 구현

--- src/main/java/sssdev/tcc/domain/comment/service/CommentService.java
+++ src/main/java/sssdev/tcc/domain/comment/service/CommentService.java
@@ -1,5 +1,38 @@
 package sssdev.tcc.domain.comment.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sssdev.tcc.domain.comment.domain.Comment;
+import sssdev.tcc.domain.comment.dto.response.CommentResponse;
+import sssdev.tcc.domain.comment.repository.CommentLikeRepoisoty;
+import sssdev.tcc.domain.comment.repository.CommentRepository;
+import sssdev.tcc.global.common.dto.LoginUser;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
 
+    private final CommentRepository commentRepository;
+    private final CommentLikeRepoisoty commentLikeRepoisoty;
+
+    public List<CommentResponse> getCommentsNonLogin(Long postId) {
+        List<Comment> commentList = commentRepository.findAllByPostId(postId);
+        List<CommentResponse> responseList = new ArrayList<>();
+
+        for (Comment comment : commentList) {
+            CommentResponse commentResponse = CommentResponse.builder()
+                .writer(comment.getUser().getUsername())
+                .content(comment.getContent())
+                .build();
+            responseList.add(commentResponse);
+        }
+
+        return responseList;
+    }
+
+    public List<CommentResponse> getCommentsLogin(Long postId, LoginUser loginUser) {
+        return null;
+    }
 }

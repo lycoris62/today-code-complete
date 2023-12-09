9e0780f4a698c57d5bec551d5f6cc5c5cfd2cec3
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-09 20:43:46 +0900

    refactory: 중복 코드 제거 및 메소드 이름 변경

--- src/main/java/sssdev/tcc/domain/comment/controller/CommentController.java
+++ src/main/java/sssdev/tcc/domain/comment/controller/CommentController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sssdev.tcc.domain.comment.dto.request.CommentCreateRequest;
 import sssdev.tcc.domain.comment.dto.request.CommentRequest;
 import sssdev.tcc.domain.comment.dto.response.CommentResponse;
 import sssdev.tcc.domain.comment.service.CommentService;
@@ -26,15 +28,11 @@ public class CommentController {
     @GetMapping("/comments")
     public ResponseEntity<?> getComments(HttpServletRequest servletRequest,
         @RequestBody CommentRequest request) {
-        if (!statusUtil.loginStatus(servletRequest)) {
-            List<CommentResponse> responseList = commentService.getCommentsNonLogin(request.postId());
-            return ResponseEntity.ok(
-                RootResponse.builder().code("200")
-                    .message("해당 게시물의 모든 댓글을 가져왔습니다.")
-                    .data(responseList).build());
+        LoginUser loginUser = null;
+        if (statusUtil.isLogin(servletRequest)) {
+            loginUser = statusUtil.getLoginUser(servletRequest);
         }
-        LoginUser loginUser = statusUtil.getLoginUser(servletRequest);
-        List<CommentResponse> responseList = commentService.getCommentsLogin(request.postId(), loginUser);
+        List<CommentResponse> responseList = commentService.getComments(request.postId(), loginUser);
         return ResponseEntity.ok(
             RootResponse.builder().code("200")
                 .message("해당 게시물의 모든 댓글을 가져왔습니다.")

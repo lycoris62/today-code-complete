d015baeecccd13ba793e5013c4014d8ad9d3674c
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-10 14:42:32 +0900

    feature: 댓글 수정 Controller API 구현

--- src/main/java/sssdev/tcc/domain/comment/controller/CommentController.java
+++ src/main/java/sssdev/tcc/domain/comment/controller/CommentController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sssdev.tcc.domain.comment.dto.request.CommentCreateRequest;
+import sssdev.tcc.domain.comment.dto.request.CommentModifyRequest;
 import sssdev.tcc.domain.comment.dto.request.CommentRequest;
 import sssdev.tcc.domain.comment.dto.response.CommentResponse;
 import sssdev.tcc.domain.comment.service.CommentService;
@@ -41,16 +44,32 @@ public class CommentController {
 
     @PostMapping("/comments")
     public ResponseEntity<?> createComments(
-        @RequestBody CommentCreateRequest requestDto,
+        @RequestBody CommentCreateRequest request,
         HttpServletRequest servletRequest) {
 
         LoginUser loginUser = statusUtil.getLoginUser(servletRequest);
-        CommentResponse commentResponse = commentService.createComments(loginUser, requestDto);
+        CommentResponse commentResponse = commentService.createComments(loginUser, request);
 
         return ResponseEntity.ok(RootResponse.builder()
             .code("200")
             .message("댓글 생성 성공")
             .data(commentResponse)
             .build());
+    }
+
+    @PatchMapping("/comments/{id}")
+    public ResponseEntity<?> modifyComments(@PathVariable(name = "id") Long id,
+        @RequestBody CommentModifyRequest request,
+        HttpServletRequest servletRequest) {
+
+        LoginUser loginUser = statusUtil.getLoginUser(servletRequest);
+        CommentResponse response = commentService.modifyComments(id, request, loginUser);
+
+        return ResponseEntity.ok(RootResponse.builder()
+            .code("200")
+            .message("댓글 수정 성공")
+            .data(response)
+            .build());
+
     }
 }

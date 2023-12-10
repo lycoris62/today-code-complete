8b128e1ab2ed01931dc38e198a27505e0f61d683
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-12-10 20:45:57 +0900

    feat(commet) : updateCommentAdmin 추가
    어드민 유저 댓글 수정 추가

--- src/main/java/sssdev/tcc/domain/comment/service/CommentService.java
+++ src/main/java/sssdev/tcc/domain/comment/service/CommentService.java
@@ -24,7 +24,6 @@ import sssdev.tcc.domain.post.repository.PostRepository;
 import sssdev.tcc.domain.user.domain.User;
 import sssdev.tcc.domain.user.repository.UserRepository;
 import sssdev.tcc.global.common.dto.LoginUser;
-import sssdev.tcc.global.execption.ErrorCode;
 import sssdev.tcc.global.execption.ServiceException;
 
 @Service
@@ -41,7 +40,7 @@ public class CommentService {
         List<CommentResponse> responseList = new ArrayList<>();
         CommentResponse response;
         List<CommentLike> commentLikeList = new ArrayList<>();
-        if(loginUser != null){
+        if (loginUser != null) {
             commentLikeList = commentLikeRepoisoty.findByUserId(loginUser.id());
         }
 
@@ -49,15 +48,16 @@ public class CommentService {
 
             boolean likeStatus = false;
 
-            if(!commentLikeList.isEmpty()){
+            if (!commentLikeList.isEmpty()) {
                 for (CommentLike commentLike : commentLikeList) {
-                    if(commentLike.getComment().equals(comment)){
+                    if (commentLike.getComment().equals(comment)) {
                         likeStatus = true;
                         break;
                     }
                 }
             }
-            response = new CommentResponse(comment.getUser().getUsername(), comment.getContent(), likeStatus);
+            response = new CommentResponse(comment.getUser().getUsername(), comment.getContent(),
+                likeStatus);
             responseList.add(response);
         }
         return responseList;
@@ -98,15 +98,18 @@ public class CommentService {
     }
 
     // todo
+    @Transactional
     public AdminCommentUpdateResponse updateCommentAdmin(Long id,
         AdminCommentUpdateRequest request) {
         Comment comment = commentRepository.findById(id)
             .orElseThrow(() -> new ServiceException(NOT_EXIST_POST));
+        comment.updateComment(request.content());
         return AdminCommentUpdateResponse.builder().id(id).content(request.content()).build();
     }
 
     @Transactional
-    public CommentResponse modifyComments(Long id, CommentModifyRequest request, LoginUser loginUser) {
+    public CommentResponse modifyComments(Long id, CommentModifyRequest request,
+        LoginUser loginUser) {
         Comment comment = commentRepository.findById(id).orElseThrow(
             () -> new ServiceException(NOT_EXIST_COMMENT)
         );
@@ -117,7 +120,7 @@ public class CommentService {
 
         boolean likeStatus = false;
 
-        if(!comment.getUser().getId().equals(user.getId())) {
+        if (!comment.getUser().getId().equals(user.getId())) {
             throw new ServiceException(CHECK_USER);
         }
 
